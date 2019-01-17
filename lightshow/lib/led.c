57fb7584a5e7a9024410961fdbc64354231700f4
osboxes.org <avpronkin@edu.hse.ru>
2019-01-17 14:19:27 -0500

    Initial proj

---
+++ lightshow/lib/led.c
@@ -0,0 +1,29 @@
+#include "fsm.h"
+
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+
+void fsm_led_init(void *args){
+	/*
+	* Setting USER LED PC8 and PC9
+	*/
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+	fsm_set_state(FSM_TERM_MAIN);
+
+}
+void fsm_led_handler(void *args){
+	uint8_t *arr = (uint8_t *)args;
+
+	if(arr[0]==1){
+		//Turn on led
+		LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_9);
+	} else {
+		//Turn off led
+		LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_9);
+	}
+	arr[0] = 2;
+	memcpy(arr+1, "OK", 2);
+	fsm_set_state(FSM_TERM_RESPOND);
+}
